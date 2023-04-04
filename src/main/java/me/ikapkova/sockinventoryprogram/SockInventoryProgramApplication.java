3490da94b99037fea88bab1831f5480f6f86135f
Ирина Капкова <kapiral@rambler.ru>
2023-04-04 22:56:23 +0400

    1 commit

---
+++ src/main/java/me/ikapkova/sockinventoryprogram/SockInventoryProgramApplication.java
@@ -0,0 +1,13 @@
+package me.ikapkova.sockinventoryprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockInventoryProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockInventoryProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ikapkova/sockinventoryprogram/SockInventoryProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ikapkova.sockinventoryprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockInventoryProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
